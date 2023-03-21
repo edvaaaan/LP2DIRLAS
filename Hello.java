c9626ac9b83bd75e5d763f575ac76b2185686272
Edvan Monteiro <87432834+edvaaaan@users.noreply.github.com>
2023-03-21 15:30:49 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Laboratório de Programação 2 - Lab 1
+ * 
+ * @author Edvan Monteiro de Oliveira Filho - 121210590
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Dirlididi!");
+    }
+}
